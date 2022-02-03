bc8271167a5218c795b076c948703c9fa0009d73
William Willits <willitsw@gmail.com>
2022-02-02 21:14:13 -0500

    eslint tweak

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 import "./global-styles.css";
 
-var mountNode = document.getElementById("root");
+const mountNode = document.getElementById("root");
 
 ReactDOM.render(<App />, mountNode);
